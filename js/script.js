9c29738cf0cb129c34cdccddac3b4f39356e878b
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-06 16:15:53 +0200

    capitalized-end

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@ Consegna:
 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 3. stampa la lista ordinata alfabeticamente sia in console che in pagina
 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
-
-
-sviluppo:
 */
+
+//creiamo un'array ed una variabile per il cognome dell'utente
+
 var list = document.getElementById('list')
 var surnameList = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 var userSurname = prompt('inserisci il tuo cognome');
 
 
-surnameList.push(userSurname);
+//Trasformiamo la prima lettera del cognome dell'utente in maiusc per non disturbare l'ordine
 
+var userSurnameUp = userSurname.charAt(0).toUpperCase() + userSurname.slice(1);
+console.log(userSurnameUp);
+
+// Inseriamo il cognome dell'utente nell'array e ordiniamo il tutto
+
+surnameList.push(userSurnameUp);
 
 console.table(surnameList.sort());
 
-var display = userSurname + ' ' + (surnameList.indexOf(userSurname) + 1);
 
-surnameList.splice(surnameList.indexOf(userSurname), 1, display);
+// creiamo una variabile che comprende il cognome dell'utente e il suo indice nell'array, e la sostituiamo al cognome dell'utente già inserito
+
+var display = userSurnameUp + ' ' + (surnameList.indexOf(userSurnameUp) + 1);
+
+surnameList.splice(surnameList.indexOf(userSurnameUp), 1, display);
 
 console.log(display);
+
+//stampiamo tutto
 
 list.innerHTML = (surnameList);
